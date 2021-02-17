f915b422e417940decad04b186ef4c85c9b698c0
Antoni Wawszczak <WKM738@motorolasolutions.com>
2021-02-17 10:33:27 +0100

    Add tests to subtraction

--- RomanNumeralsTests/ProgramTests.cs
+++ RomanNumeralsTests/ProgramTests.cs
@@ -14,8 +14,15 @@ namespace RomanNumeralsTests
 
         [Test]
         [TestCase("I", 1)]
+        [TestCase("i", 1)]
         [TestCase("II", 2)]
+        [TestCase("Ii", 2)]
         [TestCase("III", 3)]
+        [TestCase("V", 5)]
+        [TestCase("C", 100)]
+        [TestCase("L", 50)]
+        [TestCase("v", 5)]
+        [TestCase("l", 50)]
         public void ShouldParseRomanStringIntoInteger(string romanValue, int integerValue)
         {
             //Arrange & Act
@@ -25,5 +32,26 @@ namespace RomanNumeralsTests
             Assert.AreEqual(integerValue, returnedValue);
 
         }
+
+        [Test]
+        [TestCase("IV", 4)]
+        [TestCase("IX", 9)]
+        [TestCase("XIV", 14)]
+        [TestCase("XIX", 19)]
+        [TestCase("XXIV", 24)]
+        [TestCase("XL", 40)]
+        [TestCase("XC",90)]
+        [TestCase("CXC",190)]
+        public void ShouldParseRomanStringIntoIntegerSubtraction(string romanValue, int integerValue)
+        {
+            //Arrange & Act
+            int returnedValue = Program.ParseRomanNumeral(romanValue);
+
+            //Assert
+            Assert.AreEqual(integerValue, returnedValue);
+
+        }
+
+        
     }
 }
