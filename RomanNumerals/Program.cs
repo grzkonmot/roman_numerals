00bd686fbc26b840eb4afce9a9f8fe090a3efe02
grzkonmot <75835200+grzkonmot@users.noreply.github.com>
2021-02-17 10:57:33 +0100

    Refactor ParseRomanNumeral for handling subtractions

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -7,11 +7,16 @@ namespace RomanNumerals
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(ParseRomanNumeral("IV"));
+            Console.WriteLine(ParseRomanNumeral("IX"));
+            Console.WriteLine(ParseRomanNumeral("XL"));
         }
 
         public static int ParseRomanNumeral(string s)
         {
             int order = 1;
+            int decaOrder = 1;
+            int hectaOrder = 1;
             int sum = 0;
             s = s.ToUpper();
 
@@ -27,17 +32,21 @@ namespace RomanNumerals
                         sum += 5;
                         break;
                     case 'X':
-                        sum += 10;
+                        sum += 10 * decaOrder;
+                        order = -1;
+                        //decaOrder = -1;
                         break;
                     case 'C':
-                        sum += 100;
+                        sum += 100 * hectaOrder;
+                        decaOrder = -1;
                         break;
                     case 'L':
-                        order = -10;
-                        sum += 50;
+                        sum += 50 * decaOrder;
+                        decaOrder = -1;
                         break;
                     case 'D':
-                        sum += 500;
+                        sum += 500 * hectaOrder;
+                        hectaOrder = -1;
                         break;
                     case 'M':
                         sum += 1000;
