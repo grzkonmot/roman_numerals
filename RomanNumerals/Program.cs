fde8f43abc2d1742fc56359175d79269d1b588a4
Antoni Wawszczak <WKM738@motorolasolutions.com>
2021-02-17 10:15:46 +0100

    First draft for roman numbrs converter

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -2,11 +2,49 @@
 
 namespace RomanNumerals
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static int ParseRomanNumeral(string s)
+        {
+            int order = 1;
+            int sum = 0;
+            s = s.ToUpper();
+
+            for (int i = 1; s.Length - i > -1; i++)
+            {
+                switch (s[^i])
+                {
+                    case 'I':
+                        sum = sum + 1 * order;
+                        break;
+                    case 'V':
+                        order = -1;
+                        sum += 5;
+                        break;
+                    case 'X':
+                        sum += 10;
+                        break;
+                    case 'C':
+                        sum += 100;
+                        break;
+                    case 'L':
+                        order = -10;
+                        sum += 50;
+                        break;
+                    case 'D':
+                        sum += 500;
+                        break;
+                    case 'M':
+                        sum += 1000;
+                        break;
+                }
+            }
+            return sum;
         }
     }
 }
